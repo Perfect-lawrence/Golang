1485a5c8a38b9350bbe1aff04e257f52e1e76f74
xiangxiaohai@b1024.cn <xiangxiaohai@b1024.cn>
2019-07-05 00:09:12 +0800

    noFuncName

---
+++ Learn_golang/Day01_14/d13_noFuncName.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 匿名函数和闭包
+// 定义一个函数它的返回值是一个函数
+// 把函数作为返回值
+func a() func() {
+	return func() { //把函数作为返回值
+		fmt.Println("闭包")
+	}
+}
+
+//一个简单的 闭包
+func b() func() {
+	name := "闭包b函数"
+	return func() {
+		fmt.Println("hello 一个简单的闭包:", name)
+	}
+}
+
+// 闭包 = 函数 + 外层变量的引用
+func ww(name string) func() {
+	return func() {
+		fmt.Println("hello 常见的闭包形式：", name)
+	}
+}
+
+// 使用闭包做文件后缀名检查的
+func makeSuffixFunc(suffix string) func(string) string {
+	return func(name string) string {
+		// import "strings"
+		if !strings.HasSuffix(name, suffix) {
+			return name + suffix
+		}
+		return name
+	}
+}
+
+// 闭包 计算
+func calc3(base int) (func(int) int, func(int) int) {
+	add := func(i int) int {
+		base += i
+		return base
+	}
+	sub := func(i int) int {
+		base -= i
+		return base
+	}
+	return add, sub
+}
+
+func main() {
+	//匿名函数
+	func() {
+		fmt.Println("匿名函数")
+	}()
+	r := a()
+
+	r() //相当于执行 a 函数内部的匿名函数
+
+	bb := b()
+	bb() // 相当于执行 b 函数
+
+	// 使用闭包做文件后缀名检查的
+	tt := ww("Golang")
+	tt()
+
+	ee := makeSuffixFunc(".txt")
+	ret := ee("闭包")
+	fmt.Println(ret)
+
+	e2 := makeSuffixFunc(".avi")
+	big := e2("bigMovie")
+	fmt.Println(big)
+
+	x, y := calc3(100)
+	ret1 := x(200) // base = 100 + 200
+	fmt.Println(ret1)
+	ret2 := y(200) // base = 300 - 200
+	fmt.Println(ret2)
+}
+
